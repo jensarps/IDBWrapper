db9655ac6c281c3ab3bf7ee7a8c63822114658c4
Jens Arps <arps@uxebu.com>
2011-11-10 11:23:03 +0100

    Add requirejs

--- example/src/app.js
+++ example/src/app.js
@@ -1,8 +1,11 @@
-var app = {
+require(['../IDBStore.js'], function(IDBStore){
+	var app = {
+		
+		tpls: {
+			row: '<tr><td>{customerid}</td><td>{lastname}</td><td>{firstname}</td></tr>'
+		}
+		
+	};
 	
-	tpls: {
-		row: '<tr><td>{customerid}</td><td>{lastname}</td><td>{firstname}</td></tr>'
-	}
-	
-	
-};
+	console.log(IDBStore);
+});
