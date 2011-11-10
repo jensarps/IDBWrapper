dd72af3f0f3d043f9da1eef332ccb5c837aa1327
Jens Arps <arps@uxebu.com>
2011-11-10 18:20:38 +0100

    Add simple CRUD example

--- example/src/app.js
+++ example/src/app.js
@@ -1,11 +1,78 @@
 require(['../IDBStore.js'], function(IDBStore){
-	var app = {
-		
-		tpls: {
-			row: '<tr><td>{customerid}</td><td>{lastname}</td><td>{firstname}</td></tr>'
-		}
-		
+	
+	var tpls = {
+		row: '<tr><td>{customerid}</td><td>{lastname}</td><td>{firstname}</td><td><button onclick="app.deleteItem({typedcustomerid});">delete</button></td></tr>',
+		table: '<table><tr><th>Customer ID</th><th>Last Name</th><th>First Name</th></tr>{content}</table>'
 	};
 	
-	console.log(IDBStore);
+	var customers;
+	
+	var nodeCache = {};
+	
+	function init(){
+		
+		customers = new IDBStore({
+			dbName: this.dbName,
+			storeName: 'customer',
+			keyPath: 'customerid',
+			autoIncrement: true,
+			onStoreReady: updateTable
+		});
+		
+		['submit', 'customerid', 'firstname', 'lastname', 'results-container'].forEach(function(id){
+			nodeCache[id] = document.getElementById(id);
+		});
+		nodeCache.submit.addEventListener('click', enterData);
+	}
+	
+	function listItems(data){
+		var content = '';
+		data.forEach(function(item){
+			item.typedcustomerid = typeof item.customerid == 'string' ? "'" + item.customerid.trim() + "'" : item.customerid;
+			content += tpls.row.replace(/\{([^\}]+)\}/g, function(_, key){
+				return item[key];
+			});
+		});
+		nodeCache['results-container'].innerHTML = tpls.table.replace('{content}', content);
+	}
+	
+	function enterData(){
+		var data = readForm();
+		customers.put(data, function(){
+			clearForm();
+			updateTable();
+		});
+	}
+	
+	function deleteItem(id){
+		customers.remove(id, updateTable);
+	}
+	
+	function readForm(){
+		var data = {};
+		['customerid','firstname','lastname'].forEach(function(id){
+			var value = nodeCache[id].value.trim();
+			if(value.length){
+				data[id] = value;
+			}
+		});
+		return data;
+	}
+	
+	function clearForm(){
+		['customerid','firstname','lastname'].forEach(function(id){
+			nodeCache[id].value = '';
+		});
+	}
+	
+	function updateTable(){
+		customers.getAll(listItems);
+	}
+	
+	window.app = {
+		deleteItem: deleteItem
+	};
+	
+	init();
+	
 });
