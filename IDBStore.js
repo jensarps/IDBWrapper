cf694e6759ea69e5411bf71b27bcb13e11691f82
Jens Arps <mail@jensarps.de>
2012-11-20 11:15:28 +0100

    Remove numeric tr/cursor types

--- IDBStore.js
+++ IDBStore.js
@@ -51,20 +51,18 @@
     this.idb = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
     this.keyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.mozIDBKeyRange;
 
-    this.consts = window.IDBTransaction || window.webkitIDBTransaction;
-    fixupConstants(this.consts, {
+    this.consts = {
       'READ_ONLY': 'readonly',
       'READ_WRITE': 'readwrite',
       'VERSION_CHANGE': 'versionchange'
-    });
+    };
 
-    this.cursor = window.IDBCursor || window.webkitIDBCursor;
-    fixupConstants(this.cursor, {
+    this.cursor = {
       'NEXT': 'next',
       'NEXT_NO_DUPLICATE': 'nextunique',
       'PREV': 'prev',
       'PREV_NO_DUPLICATE': 'prevunique'
-    });
+    };
 
     this.openDB();
   };
