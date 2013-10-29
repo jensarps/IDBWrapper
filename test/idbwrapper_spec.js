7d3fbb5fc9ad4301095bb28af4dcf4749d358120
Jens Arps <mail@jensarps.de>
2013-10-29 11:23:46 +0100

    Add basic out-of-line tests

--- test/idbwrapper_spec.js
+++ test/idbwrapper_spec.js
@@ -74,4 +74,65 @@ describe('IDBWrapper', function(){
 
   });
 
+  describe('basic CRUD, out-of-line keys', function(){
+
+    var store;
+
+    before(function(done){
+      store = new IDBStore({
+        storeName: 'spec-store--simple-out-of-line',
+        keyPath: null
+      }, done);
+    });
+
+
+    it('should store a well-formed object', function(done){
+      var data = {
+        name: 'John'
+      };
+      var id = 1;
+      store.put(id, data, function(insertId){
+        expect(insertId).to.equal(id);
+        done();
+      }, done);
+    });
+
+    it('should fetch a stored object', function(done){
+      store.get(1, function(data){
+        expect(data.name).to.equal('John');
+        done();
+      }, done);
+    });
+
+    it('should overwrite a given object', function(done){
+      var data = {
+        name: 'James'
+      };
+      var id = 1;
+      store.put(id, data, function(insertId){
+        store.get(id, function(data){
+          expect(data.name).to.equal('James');
+          done();
+        }, done);
+      }, done);
+    });
+
+    it('should delete a given object', function(done){
+      store.remove(1, function(result){
+        store.get(1, function(data){
+          expect(data).to.not.exist;
+          done();
+        }, done);
+      }, done);
+    });
+
+
+    after(function(done){
+      store.clear(function(){
+        done();
+      });
+    });
+
+  });
+
 });
