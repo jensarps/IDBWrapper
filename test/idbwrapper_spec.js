89f57bf029552410dad7a103effc1ec5222f95cc
Jens Arps <mail@jensarps.de>
2013-11-09 13:10:25 +0100

    Add tests for queries

--- test/idbwrapper_spec.js
+++ test/idbwrapper_spec.js
@@ -344,5 +344,218 @@ describe('IDBWrapper', function(){
 
   });
 
+  describe('queries', function(){
+
+    var store;
+
+    before(function(done){
+      store = new IDBStore({
+        storeName: 'spec-store-indexes',
+        indexes: [
+          { name: 'basic', keyPath: 'name', unique: false, multiEntry: false },
+          { name: 'deep', keyPath: 'address.email', unique: false, multiEntry: false },
+          { name: 'date', keyPath: 'joined', unique: false, multiEntry: false },
+          { name: 'compound', keyPath: ['name', 'age'], unique: false, multiEntry: false }
+        ]
+      }, function(){
+
+        var dataArray = [
+          {
+            id: 1,
+            name: 'John',
+            lastname: 'Doe',
+            age: 42,
+            joined: Date.parse('Aug 9, 1995'),
+            address: {
+              email: 'j.doe@example.com',
+              city: 'New Boston'
+            }
+          },
+          {
+            id: 2,
+            name: 'Joe',
+            lastname: 'Doe',
+            age: 35,
+            joined: Date.parse('Sep 21, 2004'),
+            address: {
+              email: 'joe.doe@example.com',
+              city: 'New Boston'
+            }
+          },
+          {
+            id: 3,
+            name: 'James',
+            lastname: 'Smith',
+            age: 32,
+            joined: Date.parse('Oct 10, 2010'),
+            address: {
+              email: 'j.smith@example.com',
+              city: 'New York'
+            }
+          },
+          {
+            id: 4,
+            name: 'Frank',
+            lastname: 'Miller',
+            age: 42,
+            joined: Date.parse('Nov 27, 2001'),
+            address: {
+              email: 'f.miller@example.com',
+              city: 'New York'
+            }
+          },
+          {
+            id: 5,
+            name: 'Jenna',
+            lastname: 'Doe',
+            age: 43,
+            joined: Date.parse('Jan 7, 2011'),
+            address: {
+              email: 'j.doe@example.com',
+              city: 'New Boston'
+            }
+          },
+          {
+            id: 6,
+            name: 'John',
+            lastname: 'Smith',
+            age: 47,
+            joined: Date.parse('Feb 11, 2000'),
+            address: {
+              email: 'j.smith@example.com',
+              city: 'New Boston'
+            }
+          }
+        ];
+
+        store.putBatch(dataArray, function(){
+          done();
+        });
+
+      });
+    });
+
+    it('should fetch objects using basic index (Keyrange.only)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(2);
+        done();
+      }, {
+        index: 'basic',
+        keyRange: store.makeKeyRange({
+          only: 'John'
+        })
+      });
+
+    });
+
+    it('should fetch objects using basic index (Keyrange.lower)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(3);
+        done();
+      }, {
+        index: 'basic',
+        keyRange: store.makeKeyRange({
+          lower: 'Jo'
+        })
+      });
+
+    });
+
+    it('should fetch objects using deep index (KeyRange.only)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(2);
+        done();
+      }, {
+        index: 'deep',
+        keyRange: store.makeKeyRange({
+          only: 'j.doe@example.com'
+        })
+      });
+
+    });
+
+    it('should fetch objects using deep index (KeyRange.upper + exclude)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(1);
+        done();
+      }, {
+        index: 'deep',
+        keyRange: store.makeKeyRange({
+          upper: 'j',
+          excludeUppr: true
+        })
+      });
+
+    });
+
+    it('should fetch objects using date index (KeyRange.lower)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(5);
+        done();
+      }, {
+        index: 'date',
+        keyRange: store.makeKeyRange({
+          lower: Date.parse('Jan 1, 2000')
+        })
+      });
+
+    });
+
+    it('should fetch objects using date index (KeyRange.upper)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(4);
+        done();
+      }, {
+        index: 'date',
+        keyRange: store.makeKeyRange({
+          upper: Date.parse('Jan 1, 2005')
+        })
+      });
+
+    });
+
+    it('should fetch objects using date index (KeyRange.upper + lower)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(3);
+        done();
+      }, {
+        index: 'date',
+        keyRange: store.makeKeyRange({
+          lower: Date.parse('Jan 1, 2000'),
+          upper: Date.parse('Jan 1, 2005')
+        })
+      });
+
+    });
+
+    it('should fetch objects using compound index (KeyRange.only)', function(done){
+
+      store.query(function(data){
+        expect(data.length).to.equal(1);
+        done();
+      }, {
+        index: 'compound',
+        keyRange: store.makeKeyRange({
+          only: ['John', 42]
+        })
+      });
+
+    });
+
+
+    after(function(done){
+      store.clear(function(){
+        done();
+      });
+    });
+
+  });
 
 });
