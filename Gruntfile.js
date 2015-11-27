c34de0f5af1ca24932a73649e9459c9ae23879ef
Jens Arps <mail@jensarps.de>
2015-11-27 14:00:00 +0100

    Add grunt JSHint task

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+  'use strict';
+
+  var pkg = grunt.file.readJSON('package.json');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: pkg,
+
+    jshint: {
+      all: ['Gruntfile.js', 'idbstore.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+};
