daed04056f54680f353431a4f1a85eb43331d571
Jens Arps <mail@jensarps.de>
2015-11-27 14:48:04 +0100

    Use main from package descriptor in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
     pkg: pkg,
 
     jshint: {
-      all: ['Gruntfile.js', 'idbstore.js'],
+      all: ['Gruntfile.js', pkg.main],
       options: {
         jshintrc: '.jshintrc'
       }
