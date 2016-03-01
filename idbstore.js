c90532fa5ea075e927256410d13cb8186e578808
Jens Arps <mail@jensarps.de>
2016-03-01 14:05:12 +0100

    Fix JSHint issues

--- idbstore.js
+++ idbstore.js
@@ -10,6 +10,9 @@
  */
 
 (function (name, definition, global) {
+
+  'use strict';
+
   if (typeof define === 'function') {
     define(definition);
   } else if (typeof module !== 'undefined' && module.exports) {
@@ -595,6 +598,11 @@
       } else if (dataArray.length === 0) {
         return onSuccess(true);
       }
+
+      var count = dataArray.length;
+      var called = false;
+      var hasSuccess = false;
+
       var batchTransaction = this.db.transaction([this.storeName] , this.consts.READ_WRITE);
       batchTransaction.oncomplete = function () {
         var callback = hasSuccess ? onSuccess : onError;
@@ -602,10 +610,7 @@
       };
       batchTransaction.onabort = onError;
       batchTransaction.onerror = onError;
-      
-      var count = dataArray.length;
-      var called = false;
-      var hasSuccess = false;
+
 
       var onItemSuccess = function () {
         count--;
@@ -700,6 +705,13 @@
       if (Object.prototype.toString.call(dataArray) != '[object Array]') {
         onError(new Error('dataArray argument must be of type Array.'));
       }
+
+      var keyField = options.keyField || this.keyPath;
+      var count = dataArray.length;
+      var called = false;
+      var hasSuccess = false;
+      var index = 0; // assume success callbacks are executed in order
+
       var batchTransaction = this.db.transaction([this.storeName], this.consts.READ_WRITE);
       batchTransaction.oncomplete = function () {
         if (hasSuccess) {
@@ -710,12 +722,6 @@
       };
       batchTransaction.onabort = onError;
       batchTransaction.onerror = onError;
-
-      var keyField = options.keyField || this.keyPath;
-      var count = dataArray.length;
-      var called = false;
-      var hasSuccess = false;
-      var index = 0; // assume success callbacks are executed in order
 
       var onItemSuccess = function (event) {
         var record = dataArray[index++];
@@ -839,6 +845,13 @@
       } else if (keyArray.length === 0) {
         return onSuccess([]);
       }
+
+      var data = [];
+      var count = keyArray.length;
+      var called = false;
+      var hasSuccess = false;
+      var result = null;
+
       var batchTransaction = this.db.transaction([this.storeName] , this.consts.READ_ONLY);
       batchTransaction.oncomplete = function () {
         var callback = hasSuccess ? onSuccess : onError;
@@ -846,12 +859,6 @@
       };
       batchTransaction.onabort = onError;
       batchTransaction.onerror = onError;
-
-      var data = [];
-      var count = keyArray.length;
-      var called = false;
-      var hasSuccess = false;
-      var result = null;
 
       var onItemSuccess = function (event) {
         if (event.target.result || arrayType == 'dense') {
@@ -1345,7 +1352,7 @@
 
   /** helpers **/
   var empty = {};
-  var mixin = function (target, source) {
+  function mixin (target, source) {
     var name, s;
     for (name in source) {
       s = source[name];
@@ -1354,7 +1361,7 @@
       }
     }
     return target;
-  };
+  }
 
   IDBStore.prototype = proto;
   IDBStore.version = proto.version;
